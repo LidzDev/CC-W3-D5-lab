58ee5621d6547fb248313c1d1d37306a8c6ecc53
LidzDev <lydia.cordell@gmail.com>
2023-09-08 16:01:02 +0100

    book list showing - started on isbn page

--- controllers/library_controller.py
+++ controllers/library_controller.py
@@ -1,7 +1,14 @@
 from flask import render_template, Blueprint
+from models.library import books
+from models.book import Book
 
 library_blueprint = Blueprint("library", __name__)
 
-@library_blueprint.route('/library')
+@library_blueprint.route("/library")
 def index():
-    return render_template('index.jinja')
+    return render_template("index.jinja", title="Lydia's Library", books=books)
+
+@library_blueprint.route("/library/<isbn>")
+def show_isbn(isbn):
+    title = "Details for " + isbn
+    return render_template("isbn.jinja", title = title, isbn = isbn)
--- models/library.py
+++ models/library.py
@@ -1,8 +1,8 @@
 from models.book import Book
 
-book1 = Book('Programming Perl', 'Larry Wall', 'Programming')
-book2 = Book('Unix Power Tools', 'Jerry Peek', 'Programming')
-book3 = Book('Mastering Regular Expressions', 'Jeffrey Friedl', 'Programming')
+book1 = Book("Programming Perl", "Larry Wall", "Programming", "9780596004927")
+book2 = Book("Unix Power Tools", "Jerry Peek", "Programming", "9780679790730" )
+book3 = Book("Mastering Regular Expressions", "Jeffrey Friedl", "Programming", "9781565922570")
 
 books = (book1, book2, book3)
 
