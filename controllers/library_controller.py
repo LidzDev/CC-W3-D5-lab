1ca28a267393d644ae6d9fcc7db17971333871ed
LidzDev <lydia.cordell@gmail.com>
2023-09-08 17:07:21 +0100

    isbn page functionality added

--- controllers/library_controller.py
+++ controllers/library_controller.py
@@ -1,6 +1,6 @@
 from flask import render_template, Blueprint
 from models.library import books
-from models.book import Book
+# from models.book import Book
 
 library_blueprint = Blueprint("library", __name__)
 
@@ -10,5 +10,11 @@ def index():
 
 @library_blueprint.route("/library/<isbn>")
 def show_isbn(isbn):
-    title = "Details for " + isbn
-    return render_template("isbn.jinja", title = title, isbn = isbn)
+    title = "Details for isbn " + isbn
+    for book in books:
+        if book.isbn == isbn:
+         return render_template("isbn.jinja", title = title, isbn = isbn, book = book)
+    title = "Unfortunately isbn " + isbn + " is unavailable."
+    return render_template("isbn.jinja", title =title, isbn = isbn, book=book)
+
+#  sending in the last book in the for loop, we are not going to use it but jinja is not happy without
